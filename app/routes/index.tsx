a7f62170c3fa68909a2590c8fa3f006236710ef5
Omer Karakus <omer.karakus@lonres.com>
2023-03-28 12:47:19 +0100

    add link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <div>
       <h1>Welcome to Remix</h1>
+      <Link to="/demo">Demo Page</Link>
     </div>
   );
 }
